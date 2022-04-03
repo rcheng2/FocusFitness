0a5e67e57e9c1f13e84b396b4588b378af0d243b
Arshad-Zaman <arshadrizzzaman@gmail.com>
2022-04-02 22:08:29 -0400

    save record to database

--- app.py
+++ app.py
@@ -1,12 +1,25 @@
 # pylint: disable = import-error, invalid-envvar-default, unused-import
 """ File to run heroku app """
-
-import random
 import os
+from dotenv import find_dotenv, load_dotenv
 import flask # type: ignore
+from flask_sqlalchemy import SQLAlchemy
+
+load_dotenv(find_dotenv())
 
 app = flask.Flask(__name__)
+app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
+app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+db = SQLAlchemy(app)
 
+class Record(db.Model):
+    """ Table for exercise records """
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80))
+    duration = db.Column(db.Integer)
+    weight = db.Column(db.Integer)
+    calories_burned = db.Column(db.String())
 
 @app.route("/")
 def index():
@@ -19,18 +32,28 @@ def calculate():
     if flask.request.method == 'POST':
         duration = int(flask.request.values.get("duration"))
         weight = int(flask.request.values.get("weight"))
-        type = flask.request.values.get("type")
+        exercise_type = flask.request.values.get("exercise_type")
 
-        if type is "cardio":
-            met = 4
+        if exercise_type == "cardio":
+            met = 7
+        elif exercise_type == "weightlifting":
+            met = 5
         else:
-            met = 2
-        
-        calories_burned = (duration * met * weight) / 200
-        print(calories_burned)
+            met = 3
+
+        calories_burned = duration * ( met * 3.5 * weight) / 200
+
+        new_record = Record(username="username",
+        duration=duration,
+        weight=weight,
+        calories_burned=calories_burned)
+
+        db.session.add(new_record)
+        db.session.commit()
+
         return flask.render_template("index.html", calories_burned=calories_burned)
 
-    else:
-        return flask.render_template("index.html")
+    return flask.render_template("index.html")
 
-app.run(debug=True)
+if __name__ == "__main__":
+    app.run(debug=True)
